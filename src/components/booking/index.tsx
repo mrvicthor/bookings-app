a490b944f44f7b997c8048e15762e80b94c3e616
Victor Eleanya <victoreleanya89@gmail.com>
2022-07-29 00:05:22 +0100

    changed table style

--- src/components/booking/index.tsx
+++ src/components/booking/index.tsx
@@ -4,6 +4,7 @@ import { AlertMessage } from "@/components/index";
 import { BiDetail } from "react-icons/bi";
 import { MdDelete } from "react-icons/md";
 import { useRouter } from "next/router";
+import { capitalizeFirstLetter } from "@/hooks/capitalize";
 
 const Booking = () => {
   const router = useRouter();
@@ -15,64 +16,79 @@ const Booking = () => {
   //   <AlertMessage toggleMessage={toggleMessage} />;
   // }, 1500);
   return (
-    <div>
-      <table className="border-collapse border border-slate-400 w-full">
-        <thead>
-          <tr>
-            <th className="border border-slate-300  px-4 py-2">S/N</th>
-            <th className="border border-slate-300 px-4 py-2">Full Name</th>
-            <th className="border border-slate-300 px-4 py-2">Item</th>
-            <th className="border border-slate-300 px-4 py-2">Fault</th>
-            <th className="border border-slate-300 px-4 py-2">Comment</th>
-            <th className="border border-slate-300 px-4 py-2">Repair Cost</th>
-            <th className="border border-slate-300 px-4 py-2"></th>
-            <th className="border border-slate-300 px-4 py-2"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {bookings?.map((booking) => (
-            <tr key={booking.id}>
-              <td className="border border-slate-300 px-4 py-2">
-                {booking.id}
-              </td>
-              <td className="border border-slate-300 px-4 py-2">
-                {booking.firstName} {booking.lastName}
-              </td>
-              <td className="border border-slate-300 px-4 py-2">
-                {booking.item}
-              </td>
-              <td className="border border-slate-300 px-4 py-2">
-                {booking.fault}
-              </td>
-              <td className="border border-slate-300 px-4 py-2">
-                {booking.engineerReport}
-              </td>
-              <td className="border border-slate-300 px-4 py-2">
-                £ {booking.hardwareInstallation + booking.softwareInstallation}
-              </td>
-              <td className="border border-slate-300 px-4 py-2">
-                <button
-                  onClick={() => {
-                    router.push({
-                      pathname: `/booking/[id]`,
-                      query: { id: booking.id },
-                    });
-                  }}
-                  className="flex items-center gap-2 bg-[#2304FB] px-4 py-1 rounded text-white hover:scale-110 hover:bg-[#042444] transition duration-300 ease-out hover:ease-in"
-                >
-                  {" "}
-                  <BiDetail /> Details
-                </button>
-              </td>
-              <td className="border border-slate-300 px-4 py-2">
-                <button className="flex items-center gap-2 bg-[#EB1018] px-4 py-1 rounded text-white hover:scale-110 hover:bg-[#8C0C0C] transition duration-300 ease-out hover:ease-in">
-                  <MdDelete /> Delete
-                </button>
-              </td>
+    <div className="py-8 w-full">
+      <div className="shadow overflow-hidden rounded border-b border-gray-200">
+        <table className="min-w-full bg-white">
+          <thead className="bg-gray-800 text-white">
+            <tr>
+              <th className=" text-left py-3 px-3 uppercase font-semibold text-sm">
+                S/N
+              </th>
+              <th className=" text-left py-3 px-3 uppercase font-semibold text-sm">
+                First Name
+              </th>
+              <th className=" text-left py-3 px-3 uppercase font-semibold text-sm">
+                Last Name
+              </th>
+              <th className=" text-left py-3 px-4 uppercase font-semibold text-sm">
+                Item
+              </th>
+              <th className=" text-left py-3 px-4 uppercase font-semibold text-sm">
+                Fault
+              </th>
+              <th className="text-left py-3 px-4 uppercase font-semibold text-sm">
+                Comment
+              </th>
+              <th className="text-left py-3 px-4 uppercase font-semibold text-sm">
+                Repair Cost
+              </th>
+              <th className="text-left py-3 px-4 uppercase font-semibold text-sm"></th>
+              <th className="text-left py-3 px-4 uppercase font-semibold text-sm"></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="text-gray-700">
+            {bookings?.map((booking) => (
+              <tr key={booking.id}>
+                <td className=" text-left py-3 px-4">{booking.id}</td>
+                <td className=" text-left py-3 px-4">
+                  {capitalizeFirstLetter(booking.firstName)}
+                </td>
+                <td className=" text-left py-3 px-4">
+                  {capitalizeFirstLetter(booking.lastName)}
+                </td>
+                <td className="text-left py-3 px-4">{booking.item}</td>
+                <td className="text-left py-3 px-4">{booking.fault}</td>
+                <td className="text-left py-3 px-4">
+                  {booking.engineerReport}
+                </td>
+                <td className="text-left py-3 px-4">
+                  £{" "}
+                  {booking.hardwareInstallation + booking.softwareInstallation}
+                </td>
+                <td className="text-left py-3 px-4">
+                  <button
+                    onClick={() => {
+                      router.push({
+                        pathname: `/booking/[id]`,
+                        query: { id: booking.id },
+                      });
+                    }}
+                    className="flex items-center gap-2 bg-[#2304FB] px-4 py-1 rounded text-white hover:scale-110 hover:bg-[#042444] transition duration-300 ease-out hover:ease-in"
+                  >
+                    {" "}
+                    <BiDetail /> Details
+                  </button>
+                </td>
+                <td className="text-left py-3 px-4">
+                  <button className="flex items-center gap-2 bg-[#EB1018] px-4 py-1 rounded text-white hover:scale-110 hover:bg-[#8C0C0C] transition duration-300 ease-out hover:ease-in">
+                    <MdDelete /> Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
