5e1d29de63b884b231ae179fcb69614bc1d3bb13
Victor Eleanya <victoreleanya89@gmail.com>
2022-09-02 22:59:35 +0100

    added animation and glass effect to create booking page

--- src/pages/createBooking.tsx
+++ src/pages/createBooking.tsx
@@ -1,6 +1,8 @@
 import { useForm, useController, UseControllerProps } from "react-hook-form";
 import { useRouter } from "next/router";
 import { trpc } from "../utils/trpc";
+import { Button } from "@/components/index";
+import { AnimatePresence, motion } from "framer-motion";
 
 type FormInput = {
   FirstName: string;
@@ -68,104 +70,114 @@ const CreateBooking = () => {
   };
 
   return (
-    <div className="bg-[#C8C8C8] py-10">
-      <h1 className="text-center  text-2xl font-bold">Booking Form</h1>
-      <form
-        className="mx-auto max-w-5xl px-6 py-4 space-y-3"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <div className="flex gap-8">
-          <InputField
-            control={control}
-            name="FirstName"
-            rules={{ required: true }}
-          />
-          <InputField
-            control={control}
-            name="LastName"
-            rules={{ required: true }}
-          />
-        </div>
-        <div className="flex gap-8">
-          <InputField
-            control={control}
-            name="Email"
-            rules={{ required: true }}
-          />
-          <InputField
-            control={control}
-            name="Phone"
-            rules={{ required: true }}
-          />
-        </div>
-        <div>
-          <InputField
-            name="Street"
-            control={control}
-            rules={{ required: true }}
-          />
-        </div>
-        <div className="flex gap-8">
-          <InputField
-            name="City"
-            control={control}
-            rules={{ required: true }}
-          />
-          <InputField
-            name="PostCode"
-            control={control}
-            rules={{ required: true }}
-          />
-        </div>
-        <div className="flex gap-8">
-          <InputField
-            name="Fault"
-            control={control}
-            rules={{ required: true }}
-          />
-          <InputField
-            name="EngineerReport"
-            control={control}
-            rules={{ required: true }}
-          />
-        </div>
-        <div className="flex gap-8">
-          <InputField
-            name="Product"
-            control={control}
-            rules={{ required: true }}
-          />
-          <InputField
-            name="ItemModel"
-            control={control}
-            rules={{ required: true }}
-          />
-          <InputField
-            name="Brand"
-            control={control}
-            rules={{ required: true }}
-          />
-        </div>
-        <div className="flex gap-8">
-          <InputField
-            name="HardwareInstall"
-            control={control}
-            rules={{ required: true }}
-          />
-          <InputField
-            name="SoftwareInstall"
-            control={control}
-            rules={{ required: true }}
-          />
-        </div>
-
-        <button
-          type="submit"
-          className="w-[100%] bg-[#048444] rounded py-2 font-medium text-white"
+    <div className="flex justify-center items-center bg-gradient-to-r from-[#304352] to-[#d7d2cc] py-10 ">
+      <AnimatePresence exitBeforeEnter>
+        <motion.form
+          initial={{ y: 10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: -10, opacity: 0 }}
+          transition={{ duration: 1 }}
+          className="booking  max-w-4xl min-h-[80vh] px-6 py-4 space-y-3"
+          onSubmit={handleSubmit(onSubmit)}
         >
-          Submit
-        </button>
-      </form>
+          <h1 className="text-center  text-2xl font-bold">Booking Form</h1>
+
+          <div className="flex gap-8">
+            <InputField
+              control={control}
+              name="FirstName"
+              rules={{ required: true }}
+            />
+            <InputField
+              control={control}
+              name="LastName"
+              rules={{ required: true }}
+            />
+            <InputField
+              control={control}
+              name="Phone"
+              rules={{ required: true }}
+            />
+          </div>
+          <div className="flex gap-8">
+            <InputField
+              control={control}
+              name="Email"
+              rules={{ required: true }}
+            />
+          </div>
+
+          <div className="flex gap-8">
+            <InputField
+              name="Street"
+              control={control}
+              rules={{ required: true }}
+            />
+            <InputField
+              name="City"
+              control={control}
+              rules={{ required: true }}
+            />
+            <InputField
+              name="PostCode"
+              control={control}
+              rules={{ required: true }}
+            />
+          </div>
+          <div className="flex gap-8">
+            <InputField
+              name="Fault"
+              control={control}
+              rules={{ required: true }}
+            />
+            <InputField
+              name="EngineerReport"
+              control={control}
+              rules={{ required: true }}
+            />
+            <InputField
+              name="Product"
+              control={control}
+              rules={{ required: true }}
+            />
+            <InputField
+              name="ItemModel"
+              control={control}
+              rules={{ required: true }}
+            />
+          </div>
+          <div className="flex gap-8">
+            <InputField
+              name="Brand"
+              control={control}
+              rules={{ required: true }}
+            />
+            <InputField
+              name="HardwareInstall"
+              control={control}
+              rules={{ required: true }}
+            />
+            <InputField
+              name="SoftwareInstall"
+              control={control}
+              rules={{ required: true }}
+            />
+          </div>
+          <div className="flex gap-8">
+            <Button
+              value="Back"
+              styles="border border-black hover:bg-black hover:text-white w-[50%] ease-in duration-300"
+              handleClick={() => router.push("/dashboard")}
+            />
+            <button
+              type="submit"
+              className="w-[50%] bg-[#048444] rounded py-2 font-medium text-white hoverAnimation relative z-[1]"
+            >
+              Submit
+            </button>
+          </div>
+        </motion.form>
+      </AnimatePresence>
     </div>
   );
 };
